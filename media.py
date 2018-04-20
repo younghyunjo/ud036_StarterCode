125e36e785c1ac3f38147e856971c709a0975df6
Younghyun Jo <younghyunjo99@gmail.com>
2018-04-21 07:40:01 +0900

    Add moee class description

--- media.py
+++ media.py
@@ -1,5 +1,10 @@
 class Movie():
-    """ This class represent movie information """
+    """ This class represent movie information
+    Movie classs needs three arguments.
+    title is movie`s title.
+    poster_image is poster_image url.
+    youtube_url is movie trailer youtube url.
+    """
 
     def __init__(self, title, poster_image, youtube_url):
         self.title = title
